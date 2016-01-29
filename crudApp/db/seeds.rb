993d06ace7f893aea14dba88efb8c6caa7e30d7b
Terrell Quillin <terrellq@gmail.com>
2016-01-28 20:24:13 -0500

    Gets create and read functioning.

--- crudApp/db/seeds.rb
+++ crudApp/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.create({
+    username: 'Tiff',
+    description: 'Hey guys, so I really want to play House of the rising sun. I think it will be fun and there is enough of us to make it our own. See you soon.'
+  })
